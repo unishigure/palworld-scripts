8a24277fd956d361dfa8e26d76180853106f42b2
unishigure <uni@seanut.app>
2024-04-22 14:03:56 +0900

    fix if conditions

--- scheduled_restart/scheduled_palserver_restart.sh
+++ scheduled_restart/scheduled_palserver_restart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 
-if [ -n $PAL_ADMIN_PASS ]; then
+if [ -n "$PAL_ADMIN_PASS" ]; then
     PASS=$PAL_ADMIN_PASS
 else
     echo 'Please set the "$PAL_ADMIN_PASS" environment variable.'
     exit -1
 fi
 
-if [ -n $PAL_API_PORT ]; then
+if [ -n "$PAL_API_PORT" ]; then
     PORT=$PAL_API_PORT
 else
     PORT=8212
@@ -18,7 +18,7 @@ curl --basic -u admin:$PASS \
     -X POST "http://localhost:$PORT/v1/api/announce" \
     -H 'Content-Type: application/json' \
     --data-raw '{
-        "message": "The server will restart after 30 minutes."
+        "message": "('"$(date "+%T")"') The server will restart after 30 minutes."
     }'
 sleep 1200
 
@@ -27,7 +27,7 @@ curl --basic -u admin:$PASS \
     -X POST "http://localhost:$PORT/v1/api/announce" \
     -H 'Content-Type: application/json' \
     --data-raw '{
-        "message": "The server will restart after 10 minutes."
+        "message": "('"$(date "+%T")"') The server will restart after 10 minutes."
     }'
 sleep 300
 
@@ -36,7 +36,7 @@ curl --basic -u admin:$PASS \
     -X POST "http://localhost:$PORT/v1/api/announce" \
     -H 'Content-Type: application/json' \
     --data-raw '{
-        "message": "The server will restart after 5 minutes."
+        "message": "('"$(date "+%T")"') The server will restart after 5 minutes."
     }'
 sleep 120
 
@@ -45,7 +45,7 @@ curl --basic -u admin:$PASS \
     -X POST "http://localhost:$PORT/v1/api/announce" \
 -H 'Content-Type: application/json' \
     --data-raw '{
-        "message": "The server will restart after 3 minutes."
+        "message": "('"$(date "+%T")"') The server will restart after 3 minutes."
     }'
 sleep 120
 
@@ -55,7 +55,7 @@ curl --basic -u admin:$PASS \
     -H 'Content-Type: application/json' \
     --data-raw '{
         "waittime": 60,
-        "message": "Server will shutdown in 10 seconds."
+        "message": "('"$(date "+%T")"') Server will shutdown in 10 seconds."
     }'
 curl --basic -u admin:$PASS \
     -X POST "http://localhost:$PORT/v1/api/save" \
